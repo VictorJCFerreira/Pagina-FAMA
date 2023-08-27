6163a7a4edb59f867d98946a97bc64f040459928
joardam <pedrofdias@live.com>
2023-08-27 18:49:10 -0300

    Calcula os dias decorridos e altera o Json

--- Fama-api/obras.js
+++ Fama-api/obras.js
@@ -3,24 +3,30 @@ const { json } = require('stream/consumers');
 
 //calcula dias decorridos e dias atrasados
 
-fs.readFile('./obras.json', 'utf8', (err, info) => {
+
+const caminhoArquivo = './obras.json';
+fs.readFile(caminhoArquivo, 'utf8', (err, jsonContent) => {
   if (err) {
     console.error('Erro:', err);
     return;
   }
   const { differenceInDays } = require('date-fns');  
-  const jsonInfo = JSON.parse(info);
+  const jsonObject = JSON.parse(jsonContent);
   const dataAtual = new Date();
 
-  const dataDeInicio = jsonInfo.obra1.datas.dataDeInicio;
+  const dataDeInicio = jsonObject.obra1.datas.dataDeInicio;
   const diaDeInicio = dataDeInicio.dia;
   const mesDeInicio = dataDeInicio.mes;
   const anoDeInicio = dataDeInicio.ano;
   
-  const dataInicioProjeto = new Date(anoDeInicio, mesDeInicio - 1, diaDeInicio );
+  const dataInicioProjeto = new Date(anoDeInicio, mesDeInicio - 1 , diaDeInicio ); /* o -1 ocorre pois mes inicial é 0 para a biblioteca */
 
   const diferencaDias = differenceInDays(dataAtual, dataInicioProjeto);
 
-  console.log(`A diferença de dias absolutos entre a data de início e a data atual é: ${diferencaDias} dias`); 
+  jsonObject.obra1.datas.diasDecorridos = diferencaDias;
 
+  console.log(`A diferença de dias absolutos entre a data de início e a data atual é: ${diferencaDias} dias`); 
+  fs.writeFileSync(caminhoArquivo, JSON.stringify(jsonObject, null, 2), 'utf-8');
+
+  console.log('Valor de diasDecorridos atualizado.');
 });
