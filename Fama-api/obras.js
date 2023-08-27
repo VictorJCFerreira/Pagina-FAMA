7b723967a04cf5faaca12dd95d9ae9d2c128b9ea
joardam <pedrofdias@live.com>
2023-08-27 17:24:34 -0300

    Trabalhando nas funcionalidades das datas das obras

--- Fama-api/obras.js
+++ Fama-api/obras.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
+const { json } = require('stream/consumers');
 
-fs.readFile('./obras.json', 'utf8', (err, data) => {
+//calcula dias decorridos e dias atrasados
+
+fs.readFile('./obras.json', 'utf8', (err, info) => {
   if (err) {
     console.error('Erro:', err);
     return;
   }
+  const { differenceInDays } = require('date-fns');  
+  const jsonInfo = JSON.parse(info);
+  const dataAtual = new Date();
+
+  const dataDeInicio = jsonInfo.obra1.datas.dataDeInicio;
+  const diaDeInicio = dataDeInicio.dia;
+  const mesDeInicio = dataDeInicio.mes;
+  const anoDeInicio = dataDeInicio.ano;
   
-  const jsonData = JSON.parse(data);
-  console.log(jsonData);
-  
-  const item1Lista1 = jsonData.listaPrincipal[0].itens[0];
-  console.log(item1Lista1); // Saída: "Item 1"
- 
+  const dataInicioProjeto = new Date(anoDeInicio, mesDeInicio - 1, diaDeInicio );
+
+  const diferencaDias = differenceInDays(dataAtual, dataInicioProjeto);
+
+  console.log(`A diferença de dias absolutos entre a data de início e a data atual é: ${diferencaDias} dias`); 
+
 });
