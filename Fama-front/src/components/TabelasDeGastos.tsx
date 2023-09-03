d01690a847d0d07adee450ed5d8f29a98dccfc44
joardam <pedrofdias@live.com>
2023-09-03 12:17:11 -0300

    Adicionei opção de editar a situação

--- Fama-front/src/components/TabelasDeGastos.tsx
+++ Fama-front/src/components/TabelasDeGastos.tsx
@@ -14,6 +14,13 @@ import './TabelaDeGastos.css';
 
 function TabelaGastosEfetuados() {
   const [mostrarModal, setMostrarModal] = useState(false);
+  
+  const [mostrarModalEditarSituação, setMostrarModalEditarSituacao] = useState(false);
+
+  const [indiceClicado, setIndiceClicado] = useState<number | null>(null);
+  
+  const [situação, setSituação] = useState(""); // Estado para armazenar a situação digitada
+
   const [gastosEfetuados, setGastosEfetuados] = useState([
     { tipoDeGasto: "Pedreiro", descricao: "Mestre de obras", valor: 20, situacao: "Confirmado" },
     { tipoDeGasto: "Material", descricao: "Tijolo 1", valor: 5, situacao: "Confirmado" }
@@ -26,7 +33,31 @@ function TabelaGastosEfetuados() {
   function fecharModal() {
     setMostrarModal(false);
   }
+
+  const handleAbrirModalEditarSituacao = (index : number) => {
+    setIndiceClicado(index);
+    setMostrarModalEditarSituacao(true);
+  }
+
+  const handleFecharModalEditarSituação = () => {
+    setMostrarModalEditarSituacao(false);
+    setSituação(""); // Limpar o campo de situação ao fechar o modal
+  }
   
+  const handleConfirmarSituaçãoNova = () => {
+    if (indiceClicado !== null) {
+      // Lógica para processar a situação aqui usando o índice
+      // Usar função de mudar situação que está no js
+      console.log("Situação digitada para o índice:", indiceClicado, situação);
+    
+      // Lógica para processar a situação
+      setMostrarModalEditarSituacao(false); // Feche o modal após confirmar a situação
+      setSituação("");
+      setIndiceClicado(null); // Limpe o índice clicado
+    }
+  } 
+
+
   return (
     <IonGrid>
       <IonRow>
@@ -43,10 +74,24 @@ function TabelaGastosEfetuados() {
           <IonCol>{item.tipoDeGasto}</IonCol>
           <IonCol>{item.descricao}</IonCol>
           <IonCol>{item.valor}</IonCol>
-          <IonCol>{item.situacao}</IonCol>
+          <IonCol>{item.situacao} <IonButton onClick={() => handleAbrirModalEditarSituacao(index)} >Editar</IonButton></IonCol>
         </IonRow>
         
       ))}
+      <IonModal isOpen={mostrarModalEditarSituação} onDidDismiss={handleFecharModalEditarSituação}  className="modal-centered">
+        <div className="modal-content">
+          <IonInput
+            placeholder="Digite nova situação"
+            value={situação}
+            onIonChange={(e) => setSituação(e.detail.value!)}
+            className="custom-input"
+          />
+          <IonButton onClick={handleConfirmarSituaçãoNova}>Confirmar</IonButton>
+          <IonButton onClick={handleFecharModalEditarSituação}>Cancelar</IonButton>
+        </div>
+      </IonModal>
+
+
       <IonRow>
       <IonCol><IonButton size= "small" onClick={abrirModal}>+</IonButton></IonCol>
       </IonRow>
@@ -105,7 +150,7 @@ function TabelaGastosPendentesComBotao() {
 
   const [mostrarModal, setMostrarModal] = useState(false);
   const [situação, setSituação] = useState(""); // Estado para armazenar a situação digitada
-  const [indiceClicado, setIndiceClicado] = useState<number | null>(null);;
+  const [indiceClicado, setIndiceClicado] = useState<number | null>(null);
   
   const handleMostrarModal = (index : number) => {
     setIndiceClicado(index); // Armazene o índice
