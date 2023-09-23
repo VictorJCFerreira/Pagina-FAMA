ae71e8c3e47af3b4606d8d3305eab92470106b18
Pedro Bulle <bullepedro@gmail.com>
2023-09-23 13:09:52 -0300

    Melhorias

--- Fama-front/src/pages/AdicionarObra.tsx
+++ Fama-front/src/pages/AdicionarObra.tsx
@@ -8,13 +8,20 @@ const AdicionarObra: React.FC = () => {
     const[nomeObra, setNomeObra] = useState('')
     const[responsavel, setResponsavel] = useState('')
     const[local, setLocal] = useState('')
-    const[dataInicio, setDataInicio] = useState('')
-    const[dataEntrega, setDataEntrega] = useState('')
+    const[diaInicio, setDiaInicio] = useState('')
+    const[mesInicio, setMesInicio] = useState('')
+    const[anoInicio, setAnoInicio] = useState('')
+    const[diaEntrega, setDiaEntrega] = useState('')
+    const[mesEntrega, setMesEntrega] = useState('')
+    const[anoEntrega, setAnoEntrega] = useState('')
     const[valorPago, setValorPago] = useState('')
   
   
     function AdicionarObra(){
-      const novaObra = [nomeObra,responsavel,local,dataInicio,dataEntrega,valorPago]
+      const novaObra = [nomeObra,responsavel,local,diaInicio,mesInicio,anoInicio,diaEntrega,mesEntrega,anoEntrega,valorPago]
+      if(nomeObra == "" || responsavel == "" || local == "" || diaInicio == '' || mesInicio == '' || anoInicio == '' || diaEntrega == '' || mesEntrega == '' || anoEntrega == '' || valorPago == ''){
+
+      }else
       console.log(novaObra);
     }
     return (
@@ -25,17 +32,21 @@ const AdicionarObra: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">
-          <IonInput placeholder="Nome Obra:" onIonChange={(e: any)=> setNomeObra(e.target.value)}></IonInput>
+          <IonInput required placeholder="Nome Obra:" onIonChange={(e: any)=> setNomeObra(e.target.value)}></IonInput>
   
-          <IonInput  placeholder="Responsável:" onIonChange={(e: any)=> setResponsavel(e.target.value)}></IonInput>
+          <IonInput required placeholder="Responsável:" onIonChange={(e: any)=> setResponsavel(e.target.value)}></IonInput>
   
-          <IonInput  placeholder="Local:" onIonChange={(e: any)=> setLocal(e.target.value)}></IonInput>
+          <IonInput required placeholder="Local:" onIonChange={(e: any)=> setLocal(e.target.value)}></IonInput>
 
-          <IonInput  placeholder="Data de Inicio:    (dd-mm-aaaa)" onIonChange={(e: any)=> setDataInicio(e.target.value)}></IonInput>
+          <IonInput  required type='number' placeholder="Dia de Inicio:    " onIonChange={(e: any)=> setDiaInicio(e.target.value)}></IonInput>
+          <IonInput  required type='number' placeholder="Mês de Inicio:    " onIonChange={(e: any)=> setMesInicio(e.target.value)}></IonInput>
+          <IonInput  required type='number' placeholder="Ano de Inicio:    " onIonChange={(e: any)=> setAnoInicio(e.target.value)}></IonInput>
 
-          <IonInput  placeholder="Data de Entrega:   (dd-mm-aaaa)" onIonChange={(e: any)=> setDataEntrega(e.target.value)}></IonInput>
+          <IonInput  required type='number' placeholder="Data de Entrega:  " onIonChange={(e: any)=> setDiaEntrega(e.target.value)}></IonInput>
+          <IonInput  required type='number' placeholder="Data de Entrega:  " onIonChange={(e: any)=> setMesEntrega(e.target.value)}></IonInput>
+          <IonInput  required type='number' placeholder="Data de Entrega:  " onIonChange={(e: any)=> setAnoEntrega(e.target.value)}></IonInput>
 
-          <IonInput  placeholder="Valor Pago pela Obra:" onIonChange={(e: any)=> setValorPago(e.target.value)}></IonInput>
+          <IonInput  required type='number' placeholder="Valor Pago pela Obra:" onIonChange={(e: any)=> setValorPago(e.target.value)}></IonInput>
   
           <IonButton onClick={AdicionarObra}>Adicionar Obra</IonButton>
           </IonContent>
